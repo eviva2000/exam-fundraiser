4d2389cbff0576857967fab8eb730b60b8dcaefa
Ulrika <u.sniedze@gmail.com>
2018-11-30 15:25:13 +0100

    Adds comments to the Dashboard.jsx

--- react-dashboard/src/components/Dashboard.jsx
+++ react-dashboard/src/components/Dashboard.jsx
@@ -4,14 +4,20 @@ import Donators from "./Donators.jsx";
 import MoneyDaily from "./MoneyDaily.jsx";
 import MaterialDonations from "./MaterialDonations.jsx";
 
+//This is the parent Component
+
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { data: [] }; //Current state returns an empty array
   }
+
+  //This lifecycle method usually renders once after the component has mounted.
+  //Here we fetch the data from the external database.
 
   componentDidMount() {
     setInterval(() => {
+      // Setting the interval makes sure this state updates and fethes data after the certain interval.
       fetch(
         "https://5bffd9ef0296210013dc7e55.mockapi.io/userinfo?fbclid=IwAR0NhN7G-StoO5qVzhjNS8uGoeBNXqawSUehT9E_QzNSdoF0NktGrMVR4G0"
       ).then(res => {
@@ -23,6 +29,7 @@ export default class Dashboard extends Component {
     }, 2000);
   }
 
+  //Here we render all the children components.
   render() {
     return (
       <div>
@@ -31,10 +38,18 @@ export default class Dashboard extends Component {
             return <h1>{item.name}</h1>;
           })}
         </ul>
-        <TotalMoney />
-        <Donators />
-        <MoneyDaily />
-        <MaterialDonations />
+        <section id="totalMoney">
+          <TotalMoney />
+        </section>
+        <section id="donators">
+          <Donators />
+        </section>
+        <section id="moneyDaily">
+          <MoneyDaily />
+        </section>
+        <section id="materialDonations">
+          <MaterialDonations />
+        </section>
       </div>
     );
   }
