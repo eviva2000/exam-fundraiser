8cd76459a491937a15cc58dccf2bfe1df2846cbe
Ulrika <u.sniedze@gmail.com>
2018-11-30 15:00:14 +0100

    Fetches data for database

--- react-dashboard/src/components/Dashboard.jsx
+++ react-dashboard/src/components/Dashboard.jsx
@@ -5,9 +5,32 @@ import MoneyDaily from "./MoneyDaily.jsx";
 import MaterialDonations from "./MaterialDonations.jsx";
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      fetch(
+        "https://5bffd9ef0296210013dc7e55.mockapi.io/userinfo?fbclid=IwAR0NhN7G-StoO5qVzhjNS8uGoeBNXqawSUehT9E_QzNSdoF0NktGrMVR4G0"
+      ).then(res => {
+        res.json().then(result => {
+          console.log(result);
+          this.setState({ data: result });
+        });
+      });
+    }, 2000);
+  }
+
   render() {
     return (
       <div>
+        <ul>
+          {this.state.data.map(function(item, index) {
+            return <h1>{item.name}</h1>;
+          })}
+        </ul>
         <TotalMoney />
         <Donators />
         <MoneyDaily />
