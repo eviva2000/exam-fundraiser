b2a11a72579ba4457d56470d158600729cabd1a0
Ulrika <u.sniedze@gmail.com>
2018-12-03 13:13:42 +0100

    Violin section in Material Donations gets the number of violins donated, renders the appropriate number, number of images and titles

--- react-dashboard/src/components/Dashboard.jsx
+++ react-dashboard/src/components/Dashboard.jsx
@@ -10,8 +10,9 @@ export default class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      user: null,
-      data: []
+      user: true,
+      data: [],
+      materials: []
     }; //Current state returns an empty array
   }
 
@@ -23,11 +24,19 @@ export default class Dashboard extends Component {
       if (this.state.user) {
         // Setting the interval makes sure this state updates and fethes data after the certain interval.
         fetch(
-          "https://5bffd9ef0296210013dc7e55.mockapi.io/userinfo?fbclid=IwAR0NhN7G-StoO5qVzhjNS8uGoeBNXqawSUehT9E_QzNSdoF0NktGrMVR4G0"
+          "http://5bffd9ef0296210013dc7e55.mockapi.io/money-table?fbclid=IwAR0nDnDspJ-j42cP9m2DWn5Fwjr6PRl_EyRaVMYmCXHx8RKBld2Xswe_pOI"
         ).then(res => {
           res.json().then(result => {
             console.log(result);
             this.setState({ data: result });
+          });
+        });
+        fetch(
+          "http://5bffd9ef0296210013dc7e55.mockapi.io/material-table?fbclid=IwAR12fSE5W47qKETdsYW8Ws9T4Xxt9ch1OZ2FqttFwZy3CFl7HdDZfDo9MU0"
+        ).then(res => {
+          res.json().then(result => {
+            console.log(result);
+            this.setState({ materials: result });
           });
         });
       }
@@ -48,7 +57,10 @@ export default class Dashboard extends Component {
           <MoneyDaily />
         </section>
         <section id="materialDonations">
-          <MaterialDonations className="container" data={this.state.data} />
+          <MaterialDonations
+            className="container"
+            materials={this.state.materials}
+          />
         </section>
       </div>
     );
--- react-dashboard/src/components/Violins.jsx
+++ react-dashboard/src/components/Violins.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import Violin from "../violin.png";
 
-let Image = function statelessFunctionComponentClass() {
-  let images = [];
-  for (let i = 0; i < 5; i++) {
-    images.push(<img src={Violin} alt={"Image of violin"} />);
+// let Image = function statelessFunctionComponentClass() {
+//   let images = [];
+//   // let violinArray = [];
+//   // violinArray.push(this.props.materials.map(violins => violins.musicMaterial));
+//   // let number = violinArray.length;
+//   for (let i = 0; i < 5; i++) {
+//     images.push(<img key={Math.random()} src={Violin} alt={"Violin"} />);
+//   }
+//   return images;
+// };
+// export default Image;
+
+export default class Violins extends Component {
+  render() {
+    let images = [];
+
+    let instrumentArray = this.props.materials.map(
+      violins => violins.musicMaterial
+    );
+    console.log(instrumentArray);
+    let violinArray = instrumentArray.filter(
+      instrument => instrument === "compositions"
+    );
+
+    console.log(violinArray);
+    let number = violinArray.length;
+    for (let i = 0; i < number; i++) {
+      images.push(<img key={Math.random()} src={Violin} alt={"Violin"} />);
+    }
+    return (
+      <div>
+        {images}
+        <h1 className="instrumentNumber">Donated Guitars: {number}</h1>
+      </div>
+    );
   }
-  return images;
-};
-export default Image;
+}
