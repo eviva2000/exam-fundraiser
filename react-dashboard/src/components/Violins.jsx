20ea0c4aec563e52578b53df948b769e8a81c440
elahe ar <elahe@Brugers-MacBook-Pro-2.local>
2018-12-04 10:10:18 +0100

    comments added to material table

--- react-dashboard/src/components/Violins.jsx
+++ react-dashboard/src/components/Violins.jsx
@@ -1,18 +1,6 @@
 import React, { Component } from "react";
 import Violin from "../violin.png";
 
-// let Image = function statelessFunctionComponentClass() {
-//   let images = [];
-//   // let violinArray = [];
-//   // violinArray.push(this.props.materials.map(violins => violins.musicMaterial));
-//   // let number = violinArray.length;
-//   for (let i = 0; i < 5; i++) {
-//     images.push(<img key={Math.random()} src={Violin} alt={"Violin"} />);
-//   }
-//   return images;
-// };
-// export default Image;
-
 export default class Violins extends Component {
   render() {
     let images = [];
