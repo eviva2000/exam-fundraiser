f798e1eedf639582313081ebeeb3a0c9896ed1f4
RytisDr <r.drazdauskas@gmail.com>
2018-12-18 22:23:33 +0100

    Now signs out through firebase on sign out click.

--- react-dashboard/src/components/Header.jsx
+++ react-dashboard/src/components/Header.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
+import firebase from "../modules/firebase";
 
 export default class Header extends Component {
   render() {
+    function signOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(function(error) {
+          console.error("Sign Out Error", error);
+        });
+    }
     return (
       <div id="header">
         <h1 id="greeting">Hi Admin!</h1>
         <h1 id="signUp">Create an Admin Account</h1>
-        <h1 id="signOut">Sign Out</h1>
+        <h1 id="signOut" onClick={signOut}>
+          Sign Out
+        </h1>
       </div>
     );
   }
